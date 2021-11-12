33aaceca25593fd73ad1e9ba04e2d0064001bf7c
Yuqing Wu <ywu20@stuy.edu>
2021-11-12 14:48:29 -0500

    works

--- main.c
+++ main.c
@@ -4,30 +4,34 @@
 #include<stdlib.h>
 char ** parse_args( char * line ){
   char ** args = calloc(6, sizeof(char*));
+  line = strsep(&line, "\n");
   char* token;
   int i = 0;
-  
+
   while(token = strsep(&line," ")){
-  printf("token:%s\n", token);
-   args[i] = token;
-    printf("args[%d]: %s\n",i, args[i]);
+
+  args[i] = token;
    i++;
   }
   args[i]= NULL;
+
   return args;
 }
 
 int main(){
  char line [100];
  fgets(line, 100, stdin);
- printf("line: %s\n", line);
+
+ //printf("line: %s\n", line);
  char ** args = parse_args( line );
- 
+/*
  int i =0;
  while(args[i]){
   printf("arg[%d]: %s\n", i, args[i]);
   i++;
  }
+
+*/
  execvp(args[0], args);
  return 0;
 }
